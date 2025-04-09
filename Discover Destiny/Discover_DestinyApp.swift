772210c9c18076768c288a22da8862e6e01e2ead
AdeeshaLakshanPeiris <adeeshapiris@gmail.com>
2025-04-09 21:56:31 +0530

    Initial Commit

---
+++ Discover Destiny/Discover_DestinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discover_DestinyApp.swift
+//  Discover Destiny
+//
+//  Created by Guest User on 2025-04-09.
+//
+
+import SwiftUI
+
+@main
+struct Discover_DestinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
